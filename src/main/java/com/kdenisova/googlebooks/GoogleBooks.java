63183a31885ab73821abfff69d0d0d3be7c1cfb6
nimfa27 <kseniia.denisova@outlook.com>
2021-06-09 11:20:11 -0700

    Started to work on project structure

---
+++ src/main/java/com/kdenisova/googlebooks/GoogleBooks.java
@@ -0,0 +1,7 @@
+package com.kdenisova.googlebooks;
+
+public class GoogleBooks {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/kdenisova/googlebooks/view/Renderer.java
@@ -0,0 +1,11 @@
+package com.kdenisova.googlebooks.view;
+
+public interface Renderer {
+
+    void renderMenu();
+
+    void renderQueryRequest();
+
+    void renderReadingList();
+
+}
