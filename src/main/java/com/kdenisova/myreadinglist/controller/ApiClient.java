efc6c756efb9e3ef7c9fb22c00f5a55ce83686f1
nimfa27 <kseniia.denisova@outlook.com>
2021-06-21 20:33:23 -0700

    Organized imports

--- src/main/java/com/kdenisova/myreadinglist/controller/ApiClient.java
+++ src/main/java/com/kdenisova/myreadinglist/controller/ApiClient.java
@@ -1,7 +1,6 @@
 package com.kdenisova.myreadinglist.controller;
 
 import com.kdenisova.myreadinglist.model.BookModel;
-import org.json.JSONObject;
 
 import java.io.IOException;
 import java.util.List;
